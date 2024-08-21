724af1bbe1ec42b9ca5778b078f2af40016f1602
Rohit <rsrawat2929@gmail.com>
2024-08-22 00:40:42 +0530

    add js file

--- FirstProject/script.js
+++ FirstProject/script.js
@@ -1,1 +1,26 @@
 console.log("check")
+
+let res = {}
+
+let btn = document.getElementById("button")
+
+btn.addEventListener("click",async (e)=>{
+    e.preventDefault()
+    let key ="ema_live_CiDbIZBOPYEmyzpzrvwonS7PRYo9bqGKavYMQmXu";
+    let email = document.getElementById("username").value
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    
+    let rs =await fetch(url)
+    let res = await rs.json()
+    
+    
+    let str =''
+    for (key of Object.keys(res)){
+        str = str + `<div>${key}: ${res[key]}</div>`
+    }
+    
+    rescontainer = document.getElementById("rescontainer");
+    rescontainer.innerHTML=str
+});
+
+
